8abe833a4feda2708884e15672f9902c0d7c26b4
Tanel Tumanski <tanel.tumanski@gmail.com>
2013-07-11 17:25:00 +0300

    Filepath change

--- RaspberryGPIO/GPIO/GPIOConstants.cs
+++ RaspberryGPIO/GPIO/GPIOConstants.cs
@@ -3,6 +3,6 @@ namespace RaspberryGPIO.GPIO
 {
     class GPIOConstants
     {
-        public const string FilePath = @"C:\Users\Tanel\Desktop\raspberryTestFolder\";
+        public const string FilePath = @"/home/tanel/Desktop";
     }
 }
